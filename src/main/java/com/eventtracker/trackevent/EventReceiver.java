bbc137f0a2e9a18ac97f78baa8dbc84ff06c2849
harshtrivedi134 <hat280@nyu.edu>
2019-08-04 16:08:33 -0400

    Event-tracker Receiver class @JmsListener for the messages inserted in the queue To be removed from this project and add it in a separate microservice/project

---
+++ src/main/java/com/eventtracker/trackevent/EventReceiver.java
@@ -0,0 +1,15 @@
+package com.eventtracker.trackevent;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventReceiver {
+
+//    @JmsListener(destination = "EventTrackingQueue", containerFactory = "jmsListenerContainerFactory")
+//    public  void receiveMessage(SendEventMessageRequest sendEventMessageRequest) {
+//        System.out.println("Received <"+ sendEventMessageRequest+ ">");
+//
+//
+//
+//    }
+}
