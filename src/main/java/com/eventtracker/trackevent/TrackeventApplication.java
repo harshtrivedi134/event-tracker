f3945daaad3138b833a5c9069f60605d1742beff
harshtrivedi134 <hat280@nyu.edu>
2019-08-04 00:52:37 -0400

    Event-Tracker  Initial project structure and setup

---
+++ src/main/java/com/eventtracker/trackevent/TrackeventApplication.java
@@ -0,0 +1,14 @@
+package com.eventtracker.trackevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackeventApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TrackeventApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eventtracker/trackevent/TrackeventApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eventtracker.trackevent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrackeventApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
