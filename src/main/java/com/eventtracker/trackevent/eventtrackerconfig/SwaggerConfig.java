f7cafa2005834ec68544ad06c344d75f5b1b8039
harshtrivedi134 <hat280@nyu.edu>
2019-08-07 23:10:18 -0400

    Event-tracker reformat code

--- src/main/java/com/eventtracker/trackevent/eventtrackerconfig/SwaggerConfig.java
+++ src/main/java/com/eventtracker/trackevent/eventtrackerconfig/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return  new Docket(DocumentationType.SWAGGER_2).select()
+        return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors
                         .basePackage("com.eventtracker.trackevent.eventtrackercontroller"))
                 .paths(PathSelectors.regex("/.*"))
